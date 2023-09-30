1c2afc3a8cf2c78c0dd1e2e99565c3f0f154eac3
David Rolston <gizmola@gmail.com>
2023-09-29 23:56:05 -0700

    removed non-functional audio code

--- index.js
+++ index.js
@@ -119,42 +119,5 @@ function reset() {
        
 }
 
-/*
-function audioInit() {
-  try {
-    //let soundBuffer = null;  
-    // Fix up for prefixing
-    window.AudioContext = window.AudioContext||window.webkitAudioContext;
-    context = new AudioContext();
-  } catch(e) {
-    alert('Web Audio API is not supported in this browser');
-  }
-}
-
-function loadSound(soundBuffer, url) {
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.responseType = 'arraybuffer';
-
-  // Decode asynchronously
-  request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
-        soundBuffer = buffer;
-    }, function() {
-        console.log('Unable to load sound')
-    });
-  }
-  request.send();
-}
-
-function playSound(buffer) {
-  let source = context.createBufferSource(); // creates a sound source
-  source.buffer = buffer;                    // tell the source which sound to play
-  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-  source.start(0)                            // play the source                                            
-}
-*/
-
-// Initialize
 reset();
 
